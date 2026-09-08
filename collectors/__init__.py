35dbef99d870fdb177a3db08ec6ab164ef3f2e7e
yongyong-dbdb <c4ic79@kyonggi.ac.kr>
2026-09-08 15:31:22 +0900

    feat: export NewsCollector in v1.7.0

--- collectors/__init__.py
+++ collectors/__init__.py
@@ -1,5 +1,6 @@
+from collectors.news import NewsCollector
 from collectors.toss import TossCollector
 from collectors.reference import TossReferenceCollector
 from collectors.yahoo import YahooCollector
 
-__all__ = ["TossCollector", "TossReferenceCollector", "YahooCollector"]
+__all__ = ["NewsCollector", "TossCollector", "TossReferenceCollector", "YahooCollector"]
